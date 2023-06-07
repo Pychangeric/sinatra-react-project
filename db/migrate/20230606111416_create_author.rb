63ac2ea4bb00e61bb784b0ee6b7231dd5524ec3c
pychangeric <pychangeric@gmail.com>
2023-06-07 22:16:23 +0300

    done

--- db/migrate/20230606111416_create_author.rb
+++ db/migrate/20230606111416_create_author.rb
@@ -1,7 +1,7 @@
 class CreateAuthor < ActiveRecord::Migration[6.1]
   def change
     create_table :authors do |t|
-      t.string :name
+      t.interger :contact
       t.string :email
       t.references :book, foreign_key: true
       t.references :user, foreign_key: true
